0f00d06491a29ae0a46cc06ee7a0bd2dcfa228ca
n4to4 <n4to4k@gmail.com>
2022-01-31 14:10:18 +0900

    wip

--- unsafe-rust/src/main.rs
+++ unsafe-rust/src/main.rs
@@ -1,3 +1,6 @@
+use std::mem::MaybeUninit;
+use std::ptr::addr_of_mut;
+
 struct Role {
     name: &'static str,
     disabled: bool,
@@ -5,12 +8,16 @@ struct Role {
 }
 
 fn main() {
+    let mut role = MaybeUninit::<Role>::uninit();
+    let ptr = role.as_mut_ptr();
+
+    // https://doc.rust-lang.org/std/mem/union.MaybeUninit.html#initializing-a-struct-field-by-field
     let role = unsafe {
-        let mut role: Role = std::mem::zeroed();
-        role.name = "basic";
-        role.flag = 1;
-        role.disabled = false;
-        role
+        addr_of_mut!((*ptr).name).write("maybe-uninit");
+        addr_of_mut!((*ptr).disabled).write(false);
+        addr_of_mut!((*ptr).flag).write(1);
+
+        role.assume_init()
     };
 
     println!("{} ({} {})", role.name, role.flag, role.disabled);
