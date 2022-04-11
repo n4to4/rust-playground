0de7fac32b2233bc9404bdc6fd51af150ff39cfd
n4to4 <n4to4k@gmail.com>
2022-04-11 15:44:31 +0900

    wip

--- timeconv/src/localconv.rs
+++ timeconv/src/localconv.rs
@@ -21,6 +21,15 @@ pub fn unix2str(t: i64) -> String {
     time2str(Local.timestamp(t, 0))
 }
 
+pub fn str2time(t: &str) -> Result<DateTime<Local>, chrono::ParseError> {
+    let t = DateTime::parse_from_rfc3339(t)?.timestamp();
+    Ok(Local.timestamp(t, 0))
+}
+
+pub fn str2unix(t: &str) -> Result<i64, chrono::ParseError> {
+    Ok(DateTime::parse_from_rfc3339(t)?.timestamp())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -48,5 +57,16 @@ mod tests {
     fn test_unix2str() {
         let t = 1648771200;
         assert_eq!("2022-04-01T09:00:00+09:00", &unix2str(t));
+    }
+
+    #[test]
+    fn test_str2time() {
+        let t = str2time("2022-04-01T09:00:00+09:00").unwrap();
+        assert_eq!(1648771200, time2unix(t));
+    }
+
+    #[test]
+    fn test_str2unix() {
+        assert_eq!(1648771200, str2unix("2022-04-01T09:00:00+09:00").unwrap());
     }
 }
